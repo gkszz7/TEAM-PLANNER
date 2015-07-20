01fbe1669fc77ab59c8785537f321deaa85c5478
gkszz7 <ehddbs777@gmail.com>
2015-07-20 15:39:38 +0900

    FIRST

---
+++ finalProject/src/main/java/com/mvc/teamproject/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.mvc.teamproject.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping(value ={"/" , "home.action"}, method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		
+		return "index";
+	}
+	
+}
